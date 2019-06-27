b6c98bfaf8d4011830996718114bd52c0b4b098b
Elton Mozart <elton.mozart@gmail.com>
2019-06-26 23:39:38 -0300

    Updates

---
+++ precache-manifest.1578f77a38ef7d780a09db19aa33d299.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c1d3058f422e79127b06480ce51cbc1c",
+    "url": "/pi-frontend/index.html"
+  },
+  {
+    "revision": "b762f53c88ebcf02cf42",
+    "url": "/pi-frontend/static/css/main.795991f2.chunk.css"
+  },
+  {
+    "revision": "948f6ab7d2a1a86c843c",
+    "url": "/pi-frontend/static/js/2.5a861b61.chunk.js"
+  },
+  {
+    "revision": "b762f53c88ebcf02cf42",
+    "url": "/pi-frontend/static/js/main.369d71c7.chunk.js"
+  },
+  {
+    "revision": "ca7d9e91fbfa340459fd",
+    "url": "/pi-frontend/static/js/runtime~main.8e30383a.js"
+  },
+  {
+    "revision": "c281f024381858e041c371ad10477834",
+    "url": "/pi-frontend/static/media/Card_01.c281f024.jpg"
+  },
+  {
+    "revision": "bb49e40a220c5ff0e00bd2134d00adb8",
+    "url": "/pi-frontend/static/media/Card_02.bb49e40a.jpg"
+  },
+  {
+    "revision": "5930c2570b230021ed5b1a36b837854d",
+    "url": "/pi-frontend/static/media/Card_03.5930c257.jpg"
+  },
+  {
+    "revision": "68f8369c181f4085336a5f14cd988342",
+    "url": "/pi-frontend/static/media/Carousel02.68f8369c.jpg"
+  },
+  {
+    "revision": "ef51ba9e76f689057ac4f0c9d6870e1e",
+    "url": "/pi-frontend/static/media/Carousel03.ef51ba9e.png"
+  }
+]);
